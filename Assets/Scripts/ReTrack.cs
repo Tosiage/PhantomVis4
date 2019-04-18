fbc5ddfeab24092f6c8875d4b3c0e07745ce8739
Antonia Gerdes <antonia.gerdes@live.de>
2019-04-18 14:15:02 +0200

    initial commit: Vuforia Model Recognition working

---
+++ Assets/Scripts/ReTrack.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+using HoloToolkit;
+using HoloToolkit.Unity.InputModule;
+
+public class ReTrack : MonoBehaviour, IInputClickHandler {
+    
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+        Debug.Log("clicked");
+
+    }
+}
