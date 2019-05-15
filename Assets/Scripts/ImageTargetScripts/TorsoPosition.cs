ba62defd4cd631ac89fa4a7b450fb7382e333479
Antonia Gerdes <antonia.gerdes@live.de>
2019-05-15 17:47:22 +0200

    eventhandler für status, positionierung von sphere relativ zu kind von imagetarget

---
+++ Assets/Scripts/ImageTargetScripts/TorsoPosition.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class TorsoPosition : MonoBehaviour {
+    public bool targetStatus;
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        targetTracking(targetStatus);
+        
+    }
+
+    private void targetTracking(bool Bool)
+    {
+       /* if (Bool == true)
+        {
+            //Debug.Log("moin");
+            this.transform.position = GameObject.Find("Sphere_Astronaut").transform.position;
+            Vector3 temp = new Vector3(0.25f, 0, 0);
+            this.transform.position = this.transform.position + temp;
+        }*/
+    }
+
+}
